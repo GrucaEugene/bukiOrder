a1e7b93c65bfc8b12a7ffbb89da3d5f40b52e19c
Yevhenii Gruca <evheniihrutsa@gmail.com>
2022-07-29 22:20:16 +0300

    made faster

--- src/com/company/ReverseInteger.java
+++ src/com/company/ReverseInteger.java
@@ -1,28 +1,29 @@
 package com.company;
 
-import static java.lang.Integer.*;
-
 public class ReverseInteger {
 
     public static int reverse(int x) {
-        if(x == 0) return 0;
+        if (x == 0) return 0;
         while (x % 10 == 0) {
             x /= 10;
         }
         boolean positive = x >= 0;
         long input = Math.abs((long) x);
-        StringBuilder stringBuilder = new StringBuilder(Long.toString(input));
-        stringBuilder.reverse();
-        long result = Long.parseLong(stringBuilder.toString());
-        if ((result > (long) MAX_VALUE) || (result < (long) MIN_VALUE)) {
+        long reverse = 0;
+        while (input != 0) {
+            long remainder = input % 10;
+            reverse = reverse * 10 + remainder;
+            input = input / 10;
+        }
+        if ((reverse > (long) 0x7fffffff || (reverse < (long) 0x80000000))) {
             return 0;
         } else {
-            return positive? (int) result : (int) result * -1;
+            return positive ? (int) reverse : (int) reverse * -1;
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(reverse(-2147483648));
+        System.out.println(reverse(-2147));
 
     }
 }
